9734c057fda6a14bfb8321028fe40271a23bb7d2
Бойков Павел <pavel.boykov.95@mail.ru>
2023-01-10 22:09:03 +0700

    Ormar

--- api.py
+++ api.py
@@ -1,42 +1,28 @@
 import shutil
 from typing import List
 
-from fastapi import APIRouter, File, Form, UploadFile, Request
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, File, Form, UploadFile
 
-from schemas import UploadVideo, GetVideo, Message
+from schemas import UploadVideo, GetVideo
+from models import Video, User
 
 
 video_router = APIRouter()
 
 
 @video_router.post('/')
-async def root(title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+async def create_video(
+    title: str = Form(...), description: str = Form(...), file: UploadFile = File(...)
+):
     info = UploadVideo(title=title, description=description)
     with open(f'{file.filename}', 'wb') as buffer:
         shutil.copyfileobj(file.file, buffer)
+    user = await User.objects.first()
+    print(type(user))
+    return await Video.objects.create(user=user,
+                                      **info.dict(),
+                                      file=file.filename)
 
-    return {"file_name": file.filename, 'info': info}
-
-
-@video_router.post('/img', status_code=201)
-async def upload_image(files: List[UploadFile] = File(...)):
-    for img in files:
-        with open(f'{img.filename}', 'wb') as buffer:
-            shutil.copyfileobj(img.file, buffer)
-
-    return {"file_name": "Good"}
-
-
-@video_router.get('/video', response_model=GetVideo, responses={404: {'model': Message}})
-async def get_video():
-    user = {'id': 25, 'name': 'Name'}
-    video = {'title': 'Test', 'description': 'Description'}
-    # return GetVideo(user=user, video=video)
-    return JSONResponse(status_code=200, content=GetVideo(user=user, video=video).dict())
-
-
-@video_router.get('/test')
-async def get_test(req: Request):
-    print(req.base_url)
-    return {}
+@video_router.get('/video/{video_pk}', response_model=GetVideo, response_model_exclude={"user__users"})
+async def get_video(video_pk: int):
+    return await Video.objects.select_related('user').get(pk=video_pk)
